af90868675ed4d79a581fe2c7e60561e71b367bb
Henry Smialowicz <henry.smialowicz@gmail.com>
2026-02-10 15:34:41 -0500

    FileHandler changed into class

---
+++ src/main/java/FileHandler.java
@@ -0,0 +1,37 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class FileHandler {
+
+  private String content;
+
+  public FileHandler() {
+    this.content = "";
+  }
+
+  // String File Name -> String File Contents
+  public String handle(String filename) {
+    // Checks files only within data folder
+    Path dataDir = Paths.get("data").toAbsolutePath();
+    Path filePath = dataDir.resolve(filename).toAbsolutePath();
+
+    // If filepath tries accessing parent directories, return invalid traversal
+    if (!filePath.startsWith(dataDir)) {
+      return "Path traversal attempt detected";
+    }
+
+    // Read the file and store into a string
+    try {
+      content = Files.readString(filePath);
+    } catch (NoSuchFileException e) {
+      content = "File Not Found.";
+    } catch (IOException e) {
+      content = "Insufficient File";
+    }
+    return content;
+  }
+
+}
--- src/main/java/TopSecret.java
+++ src/main/java/TopSecret.java
@@ -3,39 +3,10 @@
  * Commmand Line Utility
  */
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.NoSuchFileException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 public class TopSecret {
 
   public static void main(String[] args) {
 
-  }
-
-  // String File Name -> String File Contents
-  private static String filehandler(String filename) {
-    // Checks files only within data folder
-    Path dataDir = Paths.get("data").toAbsolutePath();
-    Path filePath = dataDir.resolve(filename).toAbsolutePath();
-
-    // If filepath tries accessing parent directories, return invalid traversal
-    if (!filePath.startsWith(dataDir)) {
-      return "Path traversal attempt detected";
-    }
-
-    // Read the file and store into a string
-    String content;
-    try {
-      content = Files.readString(filePath);
-    } catch (NoSuchFileException e) {
-      content = "File Not Found.";
-    } catch (IOException e) {
-      content = "Insufficient File";
-    }
-    return content;
   }
 
 }
