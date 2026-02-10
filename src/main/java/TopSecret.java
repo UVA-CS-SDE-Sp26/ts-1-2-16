5977732e0e32d356a786da3985aeb59753df1783
Henry Smialowicz <henry.smialowicz@gmail.com>
2026-02-10 14:56:51 -0500

    Filehandler method written within main

--- src/main/java/TopSecret.java
+++ src/main/java/TopSecret.java
@@ -1,5 +1,40 @@
+
 /**
  * Commmand Line Utility
  */
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class TopSecret {
+
+  public static void main(String[] args) {
+
+  }
+
+  private static String filehandler(String filename) {
+    // Checks files only within data folder
+    Path dataDir = Paths.get("data").toAbsolutePath();
+    Path filePath = dataDir.resolve(filename).toAbsolutePath();
+
+    // If filepath tries accessing parent directories, return invalid traversal
+    if (!filePath.startsWith(dataDir)) {
+      return "Path traversal attempt detected";
+    }
+
+    // Read the file and store into a string
+    String content;
+    try {
+      content = Files.readString(filePath);
+    } catch (NoSuchFileException e) {
+      content = "File Not Found.";
+    } catch (IOException e) {
+      content = "Insufficient File";
+    }
+    return content;
+  }
+
 }
