56bd3eaac1660740f94314554ee834b5ddad30e8
joshuathirumalai <rpw9fb@virginia.edu>
2026-02-11 18:29:15 -0500

    Added Mockito to Build

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,11 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
+    // Mockito
+    testImplementation("org.mockito:mockito-core:5.11.0")
+    testImplementation("org.mockito:mockito-junit-jupiter:5.11.0")
+
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
