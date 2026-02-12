a47ea673cf8ad6e2e25cf3e18163e45a01d7eadf
joshuathirumalai <rpw9fb@virginia.edu>
2026-02-12 16:11:09 -0500

    Updated ProjectControl to fit Cipher

--- src/main/java/ProjectControl.java
+++ src/main/java/ProjectControl.java
@@ -1,73 +1,88 @@
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProjectControl
 {
     private FileHandler fileHandler;
+    private static final String DEFAULT_KEY = "ciphers/key.txt";
 
     public ProjectControl(FileHandler fileHandler)
     {
         this.fileHandler = fileHandler;
     }
 
-    // numbered list of files
-    public static String listFiles()
+    // list of files (internal use)
+    private static List<File> getFiles()
     {
         File folder = new File("data");
-        File[] files = folder.listFiles();
+        File[] allFiles = folder.listFiles();
 
-        if (files == null || files.length == 0)
+        List<File> files = new ArrayList<>();
+
+        StringBuilder output = new StringBuilder();
+        int count = 1;
+
+        if (allFiles != null)
+        {
+            for (File file : allFiles)
+            {
+                if (file.isFile())
+                {
+                    files.add(file);
+                }
+            }
+        }
+        return files;
+    }
+
+    public static String listFiles()
+    {
+        List<File> files = getFiles();
+        if (files.isEmpty())
         {
             return "No files available.";
         }
 
         StringBuilder output = new StringBuilder();
-        int count = 1;
 
-        for (File file : files)
+        for (int i = 0; i < files.size(); i++)
         {
-            if (file.isFile())
-            {
-                output.append(String.format("%02d", count))
-                        .append(" ")
-                        .append(file.getName())
-                        .append("\n");
-                count++;
-            }
+            output.append(String.format("%02d", i + 1))
+                    .append(" ")
+                    .append(files.get(i).getName())
+                    .append("\n");
         }
+
         return output.toString();
     }
 
     // retrieve using default key
     public String retrieve(int num)
     {
-        File folder = new File("data");
-        File[] files = folder.listFiles();
-
-        if (files == null || num < 0 || num >= files.length)
-        {
-            return "Invalid file number.";
-        }
-
-        String filename = files[num].getName();
-        String contents = fileHandler.handle(filename);
-
-        return "not done"; // change to form of "Cipher.decipher(contents)"
+        return retrieve(num, DEFAULT_KEY);
     }
 
     // retrieve using input key
     public String retrieve(int num, String key)
     {
-        File folder = new File("data");
-        File[] files = folder.listFiles();
+        List<File> files = getFiles();
 
-        if (files == null || num < 0 || num >= files.length)
+        if (num < 1 || num > files.size())
         {
             return "Invalid file number.";
         }
 
-        String filename = files[num].getName();
+        String filename = files.get(num - 1).getName();
         String contents = fileHandler.handle(filename);
 
-        return "not done"; // change to form of "Cipher.decipher(contents, key)"
+        try
+        {
+            Cipher cipher = new Cipher(key);
+            return cipher.decrypt(contents);
+        } catch (IOException | IllegalArgumentException e) {
+            return "Cipher error: " + e.getMessage();
+        }
     }
 }
