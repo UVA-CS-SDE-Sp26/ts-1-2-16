35dfc1900fdbb0a87ef61e70f84cdc5bb0cc487e
James Beck <jaybee2606@gmail.com>
2026-02-12 18:23:40 -0500

    "Finalized user interface code and created tests"

--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -4,12 +4,12 @@ public class UserInterface {
     }
 
     public static String argument_handler(String[] args){
-        if (args.length > 3) {
+        if (args.length > 2) {
             return "To many arguments";
         }
 
         //If no arguments provided, output the list of files in the data folder.
-        if (args.length == 1) {
+        if (args.length == 0) {
             return ProjectControl.listFiles();
         }
 
@@ -18,19 +18,19 @@ public class UserInterface {
         //Convert first argument from a string to the number of a file.
         int file_number;
         try {
-            file_number = Integer.parseInt(args[1]);
+            file_number = Integer.parseInt(args[0]);
         } catch (Exception e) {
             return "The first argument wasn't an integer";
         }
 
         //If one argument provided, output the contents of the specified file
         //using the default key to decipher.
-        if (args.length == 2) {
+        if (args.length == 1) {
             return pc.retrieve(file_number);
         }
 
         //If two arguments provided, output the contents of the specified file
         //using the provided key to decipher.
-        return pc.retrieve(file_number, args[2]);
+        return pc.retrieve(file_number, args[1]);
     }
 }
--- src/test/java/UserInterfaceTest.java
+++ src/test/java/UserInterfaceTest.java
@@ -6,9 +6,28 @@ public class UserInterfaceTest {
 
     @Test
     void test_argument_handler() {
-        String[] args = {"something"};
-        String output = UserInterface.argument_handler(args);
-        assertEquals("answer", output, "Correct answer should be \"answer\"");
+        String[] test1 = {"argument1", "argument2", "argument3"};
+        String output = UserInterface.argument_handler(test1);
+        assertEquals("To many arguments", output, "Correct answer should be \"To many arguments\"");
+
+        String[] test2 = {"string"};
+        output = UserInterface.argument_handler(test2);
+        assertEquals("The first argument wasn't an integer", output, "Correct answer should be \"The first argument wasn't an integer\"");
+
+        String[] test3 = {"string", "cipher key"};
+        output = UserInterface.argument_handler(test3);
+        assertEquals("The first argument wasn't an integer", output, "Correct answer should be \"The first argument wasn't an integer\"");
+
+        //This assumes that either the "data" directory doesn't exist, or that it is empty.
+        String[] test4 = {"0"};
+        output = UserInterface.argument_handler(test4);
+        assertEquals("Invalid file number.", output, "Correct answer should be \"Invalid file number.\"");
+
+        //This assumes that either the "data" directory doesn't exist, or that it is empty.
+        String[] test5 = {"0", "cipher key"};
+        output = UserInterface.argument_handler(test5);
+        assertEquals("Invalid file number.", output, "Correct answer should be \"Invalid file number.\"");
+
     }
 
 }
