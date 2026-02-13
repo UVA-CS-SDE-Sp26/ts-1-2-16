f12007d71741623bdcd6991c9d88cf53b28891ca
joshuathirumalai <rpw9fb@virginia.edu>
2026-02-13 14:48:05 -0500

    Updated ProjectControlTest

--- src/test/java/ProjectControlTest.java
+++ src/test/java/ProjectControlTest.java
@@ -29,8 +29,26 @@ class ProjectControlTest
         projectControl = new ProjectControl(fileHandler);
     }
 
+    // Retrieves a file and decrypts using default key
     @Test
-    void retrieve_works() throws IOException
+    void retrieve_default_works() throws IOException
+    {
+        File[] files = dataDir.listFiles((dir, name) ->
+                name.endsWith(".txt") || name.endsWith(".cip"));
+
+        Arrays.sort(files);
+
+        String result = projectControl.retrieve(1);
+
+        assertEquals("Carnivore, later renamed DCS1000, was a system implemented by the Federal Bureau of Investigation (FBI) that was\n" +
+                "designed to monitor email and electronic communications. It used a customizable packet sniffer that could monitor all\n" +
+                "of a target user's Internet traffic. Carnivore was implemented in October 1997. By 2005 it had been replaced with\n" +
+                "improved commercial software.", result);
+    }
+
+    // Retrieves a file and decrypts using a new key
+    @Test
+    void retrieve_newkey_works() throws IOException
     {
         File[] files = dataDir.listFiles((dir, name) ->
                 name.endsWith(".txt") || name.endsWith(".cip"));
@@ -38,12 +56,16 @@ class ProjectControlTest
         Arrays.sort(files);
 
 
-        String result = projectControl.retrieve(1);
-        String expected = projectControl.retrieve(1, "key.txt");
+        String result = projectControl.retrieve(1, "anotherkey.txt");
+        String expected = projectControl.retrieve(1, "anotherkey.txt");
 
-        assertEquals(expected, result);
+        assertEquals("Ayplgtmpc, jyrcp pclykcb BAQ9xxx, uyq y qwqrck gknjckclrcb zw rfc Dcbcpyj Zspcys md Gltcqrgeyrgml (DZG) rfyr uyq\n" +
+                "bcqgelcb rm kmlgrmp ckygj ylb cjcarpmlga amkkslgayrgmlq. Gr sqcb y asqrmkgXyzjc nyaicr qlgddcp rfyr amsjb kmlgrmp yjj\n" +
+                "md y rypecr sqcp'q Glrcplcr rpyddga. Ayplgtmpc uyq gknjckclrcb gl Marmzcp 9775. Zw 0xx3 gr fyb zccl pcnjyacb ugrf\n" +
+                "gknpmtcb amkkcpagyj qmdruypc.", result);
     }
 
+    // Retrieves a file with a number that does NOT exist
     @Test
     void retrieve_invalid_num()
     {
@@ -51,6 +73,7 @@ class ProjectControlTest
         assertEquals("Invalid file number.", result);
     }
 
+    // Tries to decrypt with a key that does NOT exist
     @Test
     void retrieve_invalid_key() throws IOException
     {
